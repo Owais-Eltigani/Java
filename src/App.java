13c43fd06af3f9cb43f2c29389b6c696e7e43a11
Owais-Eltijani <ambidextrous.in@gmail.com>
2023-08-19 17:33:23 +0530

    added black jack

--- src/App.java
+++ src/App.java
@@ -1,9 +1,72 @@
-public class App {
+
+// import java.util.Scanner;
+
+class App {
+
   public static void main(String[] args) throws Exception {
 
-    System.out.println("Hello, World!");
-    int x;
+    Cat cat = new Cat() {
+      @Override
+      public void run() {
+        System.out.println("Cat is running");
+      }
+    };
 
+    Mamol dog = new Dog();
+    Mamol dog2 = new Dog();
+
+    int cast = (int)4.5;
+
+    cat.run();
+    dog.sound();
+    dog.Alive();
+
+    enum Color {
+      RED,
+      GREEN,
+      BLUE;
+    }
+
+    Color c = Color.RED;
+
+    System.out.println(dog.equals(dog2));
   }
+}
 
+class Mamol extends Creature {
+
+  private int privat = 4;
+  protected int protect = 4;
+
+  private int feet = 4;
+
+  String name;
+
+  public Mamol() { System.out.println("New Mamol"); }
+
+  //
+  void getFeet() { System.out.println("feet: " + this.feet); }
+
+  void sound() { System.out.println("..."); }
+}
+
+class Cat implements Moves, Dance {
+
+  Cat() { System.out.println("New Cat"); }
+
+  void sound() { System.out.println("Meow-Meow"); }
+
+  public void run() { System.out.println("Cat is running"); }
+  public void dance() { System.out.println("Cat is running"); }
+}
+class Dog extends Mamol {
+
+  Dog() { System.out.println("New Dog"); }
+
+  void sound() { System.out.println("Woof-Woof"); }
+}
+
+abstract class Creature {
+
+  void Alive() { System.out.println("I'm alive"); }
 }
---
+++ src/BlackJack.java
@@ -0,0 +1,22 @@
+class Card {
+
+  private String suit;
+  private String value;
+
+  private boolean faceUp;
+
+  public Card(String suit, String value) {
+    this.suit = suit;
+    this.value = value;
+    this.faceUp = false;
+  }
+}
+
+class Deck {}
+
+class Dealer {}
+
+public class BlackJack {
+
+  public static void main(String[] args) {}
+}
